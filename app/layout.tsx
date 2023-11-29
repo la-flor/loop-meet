810655bf2028c2b980c7179dfab09a52fc6e6d15
Leif Flor <leifaflor@gmail.com>
2023-11-28 23:18:09 -0700

    Temporary Coming Soon page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
-import GlobalNav from "@components/GlobalNav";
-import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
+import clsx from "clsx";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-  title: "Next App",
+  title: "Loop-Meet",
 };
 
 export default function RootLayout({
@@ -19,20 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head />
 
-      <body className={inter.className}>
-        <Theme accentColor="cyan">
-          <header className="bg-gray-600">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-              <GlobalNav />
-            </div>
-          </header>
-          <div className="min-h-screen bg-gray-100">
-            <div className="mx-auto max-w-7xl px-2 sm:px-8 flex flex-col py-2 bg-gray-50 min-h-screen">
-              {children}
-            </div>
-          </div>
-        </Theme>
-      </body>
+      <body className={clsx(inter.className)}>{children}</body>
     </html>
   );
 }
